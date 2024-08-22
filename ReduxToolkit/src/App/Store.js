130a665be3953c2177c45ae2d834c8ad11bd9969
amanpal1586 <amanpal1586@gmail.com>
2024-08-22 10:44:22 +0530

    learn redux

---
+++ ReduxToolkit/src/App/Store.js
@@ -0,0 +1,11 @@
+import {configureStore} from '@reduxjs/toolkit'
+import todoReducer from '../features/Todo/todoSlice'
+
+
+export const store = configureStore({
+    reducer: todoReducer,
+})
+
+
+
+
