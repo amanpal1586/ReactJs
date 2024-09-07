c252b7f08e6b7da2a601653082a7d282b3565379
Aman Pal <amanpal1586@gmail.com>
2024-09-07 20:46:34 +0530

    Landing page without functionality

---
+++ LandingPage/src/components/MarketPlace.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function MarketPlace() {
+  return (
+    <div>MarketPlace</div>
+  )
+}
+
+export default MarketPlace
---
+++ LandingPage/src/main.jsx
@@ -0,0 +1,53 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import App from './App.jsx'
+import './index.css'
+import Layout from '../../react router/src/Layout.jsx'
+import Button from './Button'
+import Company from './Company'
+import Feature from './Feature'
+import Learn from './Learn'
+import MarketPlace from './MarketPlace'
+import Product from './Product'
+
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <Layout />,
+    chidren: [
+      {
+        path: '/feature',
+        element: <Feature />,
+        },
+        {
+          path: '/product',
+          element: <Product />,
+        },
+        {
+          path: '/company',
+          element: <Company />,
+        },
+        {
+          path: '/market-place',
+          element: <MarketPlace />,
+        },
+        {
+          path: '/learn',
+          element: <Learn />,
+        },
+        {
+          path: '/login',
+          element: <Login/>
+        }
+
+
+    ]
+  }
+])
+
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    <App />
+  </StrictMode>,
+)
